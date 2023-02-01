5fe866deaa25be5be5751252118e0b646c4658cd
Fail Salikhov <sff1313@yandex.ru>
2023-02-01 14:55:32 +0400

    add navigation comp

---
+++ app/src/main/java/com/example/shift/presentation/SearchAndInfoFragment.kt
@@ -0,0 +1,7 @@
+package com.example.shift.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.shift.R
+
+class SearchAndInfoFragment : Fragment(R.layout.fragment_search_info) {
+}
